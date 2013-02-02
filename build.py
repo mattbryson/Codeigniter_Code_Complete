b952449ecd552935e9a22ebb4c5713e922c7387b
Matt Bryson <matt.bryson@skinkers.com>
2013-02-02 12:36:44 +0000

    Now writes class definition to CC files

--- build.py
+++ build.py
@@ -3,16 +3,19 @@
 #
 # CI custom classes auto complete generation
 #
-import os, sys, argparse, glob, errno, shutil
+import os, sys, argparse, glob, errno, shutil, re
 from collections import namedtuple
 from stat import *
 
+#This dir location..
 self_dir = os.path.abspath(os.path.dirname(sys._getframe(0).f_code.co_filename))
+
+#Codeigniter site location (set as -c arg when running script)
 ci_dir = ''
 
 
-
-def walk_mvc (top, callback):
+# Finds the models and controllers dirs in an MVC triad location
+def findmvc (top, callback):
 	models_dir = os.path.join( top, 'models')
 	controllers_dir = os.path.join( top, 'controllers')
 	
@@ -20,18 +23,20 @@ def walk_mvc (top, callback):
 	walktree(controllers_dir, callback)
 
 
-def walk_modules(top, callback):
+# Finds all the modules loaded under an HMVC framework
+def findmodules(top, callback):
 	
 	for f in os.listdir(top):
 		pathname = os.path.join(top, f)
 		mode = os.stat(pathname)[ST_MODE]
 		if S_ISDIR(mode):
 			 # It's a directory or MVC triad, recurse into it
-			walk_mvc(pathname, callback)
+			findmvc(pathname, callback)
 		else:
 			# Unknown file type, print a message
 			print 'Ignoring %s' % pathname 
 
+# Recursively walk tree looking for php classes
 def walktree(top, callback):
     '''recursively descend the directory tree rooted at top,
        calling the callback function for each regular file'''
@@ -52,32 +57,48 @@ def walktree(top, callback):
             # Unknown file type, print a message
             print 'Ignoring %s' % pathname
 
-def parsefile(file):
-    print 'analysing', file
+# Parse a php class, and make a code hint copy of it
+def parsefile(filepath):
+#    print 'Analysing', file
     
-    #OPen and read orig file
-    #    file = open(file, 'r')
-    
-    
-
-    out_file = file.replace(ci_dir, self_dir)
-    print 'creating file', out_file
-    dirs = os.path.split(out_file)[0]
-
+    #Resolve path to new code-complete file
+    cc_filepath = filepath.replace(ci_dir, self_dir)
+#    print 'Creating file : ', cc_filepath
+    #Get the dir path for the new file
+    dirs = os.path.split(cc_filepath)[0]
+    #Make dir structure
     try: os.makedirs(dirs, 0777)
     except OSError as err:
         # Reraise the error unless it's about an already existing directory 
         if err.errno != errno.EEXIST or not os.path.isdir(dirs): 
             raise
-
-    file = open(out_file, 'w+')
-    file.close()
     
+    # Class open bracket
+    class_bracket = '{'
+    
+    #Open out files (and create the out file)
+    in_file = open(filepath, 'r')
+    out_file = open(cc_filepath, 'w+')
+ 
+    for line in in_file:
+	    if re.match("^(?!.*[\*,/,',\"]).*class .*$", line):
+			print line,
+			if class_bracket not in line:
+			    line = line+' '+class_bracket
+			
+			out_file.write(line)
+			
+
+    in_file.close()
+    out_file.close()
+
+# log errors    
 def die(msg):
     print msg
     sys.exit(1)
 
-	
+
+# main entry point.
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--codeigniter', default='../www', help='path to codeigniter installation relative to codecomplete')
@@ -90,23 +111,18 @@ def main():
     applicaiton_dir = os.path.abspath(os.path.join(self_dir, args.codeigniter, 'application'))
     modules_dir = os.path.abspath(os.path.join(self_dir, args.codeigniter, 'application', 'modules'))
 
-    print applicaiton_dir
-    print modules_dir
-    print ci_dir
-    print self_dir
-    
     generated_files = os.path.join(self_dir, 'application')
     if( os.path.exists( generated_files )):
     	shutil.rmtree( generated_files )
     
     
-    walk_mvc(applicaiton_dir, parsefile)   
-    walk_modules(modules_dir, parsefile)
+    findmvc(applicaiton_dir, parsefile)   
+    findmodules(modules_dir, parsefile)
     
 	
 	
 	
-	
+# go	
 if __name__ == '__main__':
 	main()
 
